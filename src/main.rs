8a9789402473db97f403fb606054eec2f4590718
Andrey Rodionov <rdnvndr@disroot.org>
2026-04-16 08:20:15 +0300

    Init

---
+++ src/main.rs
@@ -0,0 +1,383 @@
+use chrono::{DateTime, Local, NaiveDateTime, TimeZone};
+use regex::Regex;
+use std::env;
+use std::fs;
+use std::io;
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
+use std::process::Command;
+
+fn make_readonly(path: &Path) -> io::Result<()> {
+    if !path.exists() {
+        return Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "The file does not exists"
+        ));
+    }
+
+    let metadata = fs::metadata(path)?;
+
+    if metadata.is_dir() {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Cannot set read-only to directory"
+        ));
+    }
+
+    let mut permissions = metadata.permissions();
+
+    #[cfg(unix)]
+    permissions.set_mode(0o444);
+
+    #[cfg(windows)]
+    permissions.set_readonly(true);
+
+    fs::set_permissions(path, permissions)?;
+    Ok(())
+}
+
+fn git(args: &[&str]) -> Result<String, String> {
+    let output = Command::new("git")
+        .args(args)
+        .env("LC_ALL", "C.UTF-8")
+        .output()
+        .map_err(|e| format!("Failed to execute git: {}", e))?;
+
+    if output.status.success() {
+       String::from_utf8(output.stdout)
+           .map(|s| s.trim().to_string())
+           .map_err(|e| format!("Invalid UTF-8: {}", e))
+   } else {
+       String::from_utf8(output.stderr)
+           .map_err(|e| format!("Invalid UTF-8 in stderr: {}", e))
+   }
+}
+
+fn tf(args: &[&str]) -> Result<String, String> {
+    let output = Command::new("tf")
+        .args(args)
+        .env("LANG", "C")
+        .env("TF_ADDITIONAL_JAVA_ARGS", "-Dfile.encoding=UTF-8 -Duser.language=en -Duser.country=US")
+        .output()
+        .map_err(|e| format!("Failed to execute tf: {}", e))?;
+
+    if output.status.success() {
+        String::from_utf8(output.stdout)
+            .map(|s| s.trim().to_string())
+            .map_err(|e| format!("Invalid UTF-8: {}", e))
+    } else {
+        String::from_utf8(output.stderr)
+            .map_err(|e| format!("Invalid UTF-8 in stderr: {}", e))
+    }
+}
+
+struct Workfold {
+    workspace: String,
+    collection: String,
+    remote: String,
+    local: String,
+}
+
+fn get_workfold(toplevel: &str) -> Result<Workfold, String> {
+    let tf_workfold = tf(&["workfold",  &toplevel])?;
+    let mut workfold_lines = tf_workfold.lines();
+
+    let workspace_regex = Regex::new(r"\s*Workspace:\s*([\s\S]*)\s*").map_err(|e| e.to_string())?;
+    let workspace = workfold_lines
+        .find_map(|s| workspace_regex.captures(s))
+        .map(|caps| caps.get(1).unwrap().as_str().trim())
+        .ok_or_else(|| "Missing workspace".to_string())?;
+
+    let collection_regex = Regex::new(r"\s*Collection:\s*([\s\S]*)\s*").map_err(|e| e.to_string())?;
+    let collection = workfold_lines
+        .find_map(|s| collection_regex.captures(s))
+        .map(|caps| caps.get(1).unwrap().as_str().trim())
+        .ok_or_else(|| "Missing collection".to_string())?;
+
+    let mut path_map_expr = String::from(r"\s*(\$[^:]*)\s*:\s*(");
+    path_map_expr.push_str(&regex::escape(&toplevel));
+    path_map_expr.push_str(r")\s*");
+    let path_map_regex = Regex::new(&path_map_expr).map_err(|e| e.to_string())?;
+    let path_map: (&str, &str) = workfold_lines
+        .find_map(|s| path_map_regex.captures(s))
+        .map(|caps| (caps.get(1).unwrap().as_str().trim(), caps.get(2).unwrap().as_str().trim()))
+        .ok_or_else(|| "Missing path_map".to_string())?;
+
+    Ok(Workfold{
+        workspace: workspace.to_string(),
+        collection: collection.to_string(),
+        remote: path_map.0.to_string(),
+        local: path_map.1.to_string() })
+}
+
+fn to_date(src: &str) -> String {
+    let format = "%b %d, %Y, %I:%M:%S %p";
+    let naive_dt = NaiveDateTime::parse_from_str(src, format).unwrap();
+    let dt: DateTime<Local> = Local.from_local_datetime(&naive_dt).unwrap();
+    String::from(dt.to_rfc3339())
+}
+
+#[derive(Default)]
+struct Commit<'a> {
+    user: &'a str,
+    date: &'a str,
+    comment: Vec<&'a str>,
+    changeset: &'a str,
+}
+
+fn fetch() -> Result<String, String> {
+    let branch = git(&["branch", "--show-current"])?;
+    let stash = git(&["stash", "push", "-u"])?;
+    let toplevel = git(&["rev-parse", "--show-toplevel"])?;
+    if branch != "tfs" {
+        git(&["switch", "tfs"])?;
+    }
+
+    let workfold = get_workfold(&toplevel)?;
+
+    let mut workspace = String::from("-workspace:");
+    workspace.push_str(&workfold.workspace);
+
+    let mut collection = String::from("-collection:");
+    collection.push_str(&workfold.collection);
+
+    tf(&["workfold", "undo", "-recursive", &workfold.local])?;
+    tf(&["workfold", "uu", "-recursive", &workfold.local])?;
+
+    let vers_regex = Regex::new(r"(^[0-9]+)").map_err(|e| e.to_string())?;
+    let tf_vers = tf(&["history", &collection, "-recursive", "-stopafter:1", &workfold.remote])?;
+    let vers: &str = tf_vers
+        .lines()
+        .find_map(|s| vers_regex.captures(s))
+        .and_then(|caps| caps.get(1))
+        .map(|m| m.as_str().trim())
+        .ok_or_else(|| "Missing version".to_string())?;
+
+    let last_regex = Regex::new(r"\s*git-tf-id:\s*([0-9]+)\s*").map_err(|e| e.to_string())?;
+    let git_last = git(&["log", "-n", "1", "tfs", "--pretty=format:\"%B\"", &workfold.local])?;
+    let last: &str = git_last
+        .lines()
+        .find_map(|s| last_regex.captures(s))
+        .and_then(|caps| caps.get(1))
+        .map(|m| m.as_str().trim())
+        .ok_or_else(|| "Missing last version".to_string())?;
+
+    let rng_vers = format!("-version:{}~{}", if last.is_empty() { vers } else { last }, vers);
+    let tf_history = tf(
+        &["history", &workfold.remote, &rng_vers, "-noprompt", "-format:detailed", "-recursive"])?;
+
+    let changeset_regex = Regex::new(r"\s*Changeset:\s*([\S\s]*)\s*").map_err(|e| e.to_string())?;
+    let user_regex = Regex::new(r"\s*User:\s*([\S\s]*)\s*").map_err(|e| e.to_string())?;
+    let date_regex = Regex::new(r"\s*Date:\s*([\S\s]*)\s*").map_err(|e| e.to_string())?;
+    let comment_regex = Regex::new(r"\s*Comment:\s*").map_err(|e| e.to_string())?;
+    let items_regex = Regex::new(r"\s*Items:\s*").map_err(|e| e.to_string())?;
+    let no_ci_regex = Regex::new(r"\s***NO_CI***\s").map_err(|e| e.to_string())?;
+
+    let mut is_comment = false;
+    let mut commit = Commit::default();
+    let mut commits: Vec<Commit> = Vec::new();
+    for line in tf_history.lines() {
+        if is_comment {
+            if let Some(_) = items_regex.find(line) {
+                commits.push(commit);
+                commit = Commit::default();
+                is_comment = false
+            }
+            else
+            {
+                if let Some(_) = no_ci_regex.find(line) {
+                    continue;
+                }
+                commit.comment.push(line.trim());
+            }
+            continue;
+        }
+
+        if let Some(caps) = changeset_regex.captures(line) {
+            commit.changeset = caps.get(1)
+                .ok_or_else(|| "Missing changeset".to_string())?
+                .as_str().trim();
+            continue;
+        }
+
+        if let Some(caps) = user_regex.captures(line) {
+            commit.user = caps.get(1)
+                .ok_or_else(|| "Missing user".to_string())?
+                .as_str().trim();
+            continue;
+        }
+
+        if let Some(caps) = date_regex.captures(line) {
+            commit.date = caps.get(1)
+                .ok_or_else(|| "Missing date".to_string())?
+                .as_str().trim();
+            continue;
+        }
+
+        if let Some(_) = comment_regex.find(line) {
+            is_comment = true;
+            continue;
+        }
+    }
+
+    let git_ls_files = git(&["ls-files", "--full-name", &toplevel])?;
+    for line in git_ls_files.lines() {
+        let path = Path::new(&toplevel).join(&line);
+        make_readonly(&path).map_err(|e| e.to_string())?;
+    }
+
+    for commit in commits.iter().rev() {
+        let mut vers = String::from("-version:");
+        vers.push_str(commit.changeset);
+        tf(&["get", "-recursive", "-overwrite", &vers, "-force", &toplevel])?;
+
+        git(&["add", &toplevel])?;
+
+        let mut comment = String::from("\"");
+        comment.push_str(&commit.comment.join("\n"));
+        comment.push_str("\ngit-tf-id: ");
+        comment.push_str(commit.changeset);
+        comment.push_str("\"");
+
+        let mut date = String::from("--date=\"");
+        date.push_str(&to_date(commit.date));
+        date.push_str("\"");
+
+        let mut author = String::from("--author=\"");
+        author.push_str(commit.user);
+        author.push_str(" <noreply@topsystems.ru>\"");
+        git(&["commit", "-n", "-m", &comment, &date, &author])?;
+    }
+
+    if branch != "tfs" {
+        git(&["switch", &branch])?;
+    }
+
+    if stash != "No local changes to save" {
+        git(&["stash", "pop"])?;
+    }
+
+    Ok("Changes fetched!".to_string())
+}
+
+fn push(msg: &str) -> Result<String, String>
+{
+    let branch = git(&["branch", "--show-current"])?;
+    let toplevel = git(&["rev-parse", "--show-toplevel"])?;
+
+    let workfold = get_workfold(&toplevel)?;
+    tf(&["workfold", "undo", "-recursive", &workfold.local])?;
+
+    let last_regex = Regex::new(r".([a-z0-9]*).").map_err(|e| e.to_string())?;
+    let git_last = git(&["log", "-n", "1", "tfs", "--pretty=format:\"%H\"", &workfold.local])?;
+    let last: &str = git_last
+        .lines()
+        .find_map(|s| last_regex.captures(s))
+        .and_then(|caps| caps.get(1))
+        .map(|m| m.as_str().trim())
+        .ok_or_else(|| "Missing hash".to_string())?;
+
+    let commitbranch_regex = Regex::new(&format!("\\s*({})\\s*", branch)).map_err(|e| e.to_string())?;
+    let git_commitbranch = git(&["branch", "--contains", &last])?;
+    let commitbranch: &str = git_commitbranch
+        .lines()
+        .find_map(|s| commitbranch_regex.captures(s))
+        .and_then(|caps| caps.get(1))
+        .map(|m| m.as_str().trim())
+        .ok_or_else(|| "Missing baranch".to_string())?;
+
+    if commitbranch.is_empty() {
+        return Err(String::from("В текущую ветку не слиты изменения ветки TFS"));
+    }
+
+    let modify_regex = Regex::new(r"\s*M\s*([\S\s]*)\s*").map_err(|e| e.to_string())?;
+    let add_regex = Regex::new(r"\s*A\s*([\S\s]*)\s*").map_err(|e| e.to_string())?;
+    let del_regex = Regex::new(r"\s*D\s*([\S\s]*)\s*").map_err(|e| e.to_string())?;
+    let move_regex = Regex::new(r"\s*R\s*[0-9]*\s*([\S\s]*)\s*->\s*([\S\s]*)\s*").map_err(|e| e.to_string())?;
+    let copy_regex = Regex::new(r"\s*C\s*[0-9]*\s*([\S\s]*)\s*->\s*([\S\s]*)\s*").map_err(|e| e.to_string())?;
+
+    let git_diff = git(&["diff", "tfs", &branch, "-b", "--name-status"])?;
+    for line in git_diff.lines() {
+        if let Some(caps) = modify_regex.captures(line) {
+            let mut file = String::from(&toplevel);
+            file.push_str("/");
+            file.push_str(caps.get(1)
+                .ok_or_else(|| "Missing change".to_string())?
+                .as_str().trim());
+            tf(&["checkout", &file])?;
+            continue;
+        }
+
+        if let Some(caps) = add_regex.captures(line) {
+            let mut file = String::from(&toplevel);
+            file.push_str("/");
+            file.push_str(caps.get(1)
+                .ok_or_else(|| "Missing change".to_string())?
+                .as_str().trim());
+            tf(&["add", &file])?;
+            continue;
+        }
+
+        if let Some(caps) = del_regex.captures(line) {
+            let mut file = String::from(&toplevel);
+            file.push_str("/");
+            file.push_str(caps.get(1)
+                .ok_or_else(|| "Missing change".to_string())?
+                .as_str().trim());
+            tf(&["delete", &file])?;
+            continue;
+        }
+
+        if let Some(caps) = copy_regex.captures(line) {
+            let mut file = String::from(&toplevel);
+            file.push_str("/");
+            file.push_str(caps.get(2)
+                .ok_or_else(|| "Missing change".to_string())?
+                .as_str().trim());
+            tf(&["add", &file])?;
+            continue;
+        }
+
+        if let Some(caps) = move_regex.captures(line) {
+            let mut file1 = String::from(&toplevel);
+            file1.push_str("/");
+            file1.push_str(caps.get(1)
+                .ok_or_else(|| "Missing change".to_string())?
+                .as_str().trim());
+            let mut file2 = String::from(&toplevel);
+            file2.push_str("/");
+            file2.push_str(caps.get(2)
+                .ok_or_else(|| "Missing change".to_string())?
+                .as_str().trim());
+            tf(&["add", &file2])?;
+            tf(&["delete", &file1])?;
+            continue;
+        }
+    }
+
+    let mut comment = String::from("-comment:");
+    if msg.is_empty() {
+        comment.push_str(&git(&["log", "-n", "1", "--pretty=format:\"%B\"", &workfold.local])?);
+    } else {
+        comment.push_str("\"");
+        comment.push_str(msg);
+        comment.push_str("\"");
+    }
+
+    tf(&["checkin", &comment, &workfold.local])?;
+
+    Ok("Changes pushed!".to_string())
+}
+
+fn main() {
+    let mut args = env::args();
+    args.next().unwrap();
+
+    let command = args.next().unwrap_or("".to_string());
+    let msg = match command.as_str() {
+        "fetch" => { fetch().unwrap() }
+        "push" => { push(args.next().unwrap().as_str()).unwrap() }
+        _ => { "No command!".to_string() }
+    };
+    println!("{}", &msg);
+}
